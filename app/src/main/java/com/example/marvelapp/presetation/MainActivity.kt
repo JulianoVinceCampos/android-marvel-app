7db65916e83cc6fb9ca80bc234d1490a8bbad043
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-06 11:17:57 -0300

    setting dagger hilt in project

--- app/src/main/java/com/example/marvelapp/presetation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presetation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.marvelapp.presetation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.marvelapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
