93ff9c95725a6b7c8c101c2fb9f6a4938ab2a8ed
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-14 09:56:48 -0300

    Optimizing getCharactersUseCase class to implement an interface so we decouple the implementation more and follow stronger with SOLID patterns

--- core/src/main/java/com/example/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/com/example/core/usecase/GetCharactersUseCase.kt
@@ -5,20 +5,29 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.core.data.repository.CharactersRepository
 import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase.GetCharactersParms
 import com.example.core.usecase.base.PagingUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCharactersUseCase @Inject constructor(
-    private val charactersRepository: CharactersRepository
-) : PagingUseCase<GetCharactersUseCase.GetCharactersParms, Character>() {
+interface GetCharactersUseCase {
 
-    override fun createFlowObservable(params: GetCharactersParms): Flow<PagingData<Character>> {
-        return Pager(config = params.pagingConfig) {
-            charactersRepository.getCharacters(params.query)
-        }.flow
-    }
+    operator fun invoke(params: GetCharactersParms): Flow<PagingData<Character>>
 
     data class GetCharactersParms(val query: String, val pagingConfig: PagingConfig)
+
+}
+
+class GetCharactersUseCaseImpl @Inject constructor(
+    private val charactersRepository: CharactersRepository
+) : PagingUseCase<GetCharactersParms, Character>(),
+    GetCharactersUseCase {
+
+    override fun createFlowObservable(params: GetCharactersParms): Flow<PagingData<Character>> {
+        val pagingSource = charactersRepository.getCharacters(params.query)
+        return Pager(config = params.pagingConfig) {
+            pagingSource
+        }.flow
+    }
 
 }
