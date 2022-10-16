2d83f3c2abc344319ac397f6918efc4ccb2b67f6
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-16 10:08:36 -0300

    Creating test class CharactersFragmentTest.kt

---
+++ app/src/androidTest/java/com/example/marvelapp/presetation/characters/CharactersFragmentTest.kt
@@ -0,0 +1,96 @@
+package com.example.marvelapp.presetation.characters
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.R
+import com.example.marvelapp.extension.asJsonString
+import com.example.marvelapp.framework.di.BaseUrlMode
+import com.example.marvelapp.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@UninstallModules(BaseUrlMode::class)
+@HiltAndroidTest
+class CharactersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer().apply {
+            start(8080)
+        }
+        launchFragmentInHiltContainer<CharactersFragment>()
+    }
+
+    @Test
+    fun shouldShowCharacters_whenWiewIsCreated() {
+
+        server.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+
+        onView(
+            withId(R.id.recycler_characters)
+        ).check(
+            matches(isDisplayed())
+        )
+
+    }
+
+    @Test
+    fun shouldLoadMoreCharacters_whenNewPageIsRequested() {
+
+        with(server) {
+            enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+            enqueue(MockResponse().setBody("characters_p2.json".asJsonString()))
+        }
+
+
+        //Action
+        onView(
+            withId(R.id.recycler_characters)
+        ).perform(
+            RecyclerViewActions.scrollToPosition<CharacterViewHolder>(20)
+        )
+
+        //Assert
+        onView(
+            withText("Amora")
+        ).check(
+            matches(isDisplayed())
+        )
+
+    }
+
+    @Test
+    fun shouldShowErrorView_whenRecivesAnErrorFromApi() {
+        //Arrange
+        server.enqueue(MockResponse().setResponseCode(404))
+
+        onView(
+            withId(R.id.text_initial_loading_error)
+        ).check(
+            matches(isDisplayed())
+        )
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+}
