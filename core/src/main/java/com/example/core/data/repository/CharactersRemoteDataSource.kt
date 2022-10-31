5b4b7922083120a72f5477aa5d32fa25b1bf7fb5
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-31 14:27:03 -0300

    Creating a new fetComics method in the CharactersRemoteDataSource interface that returns a Comic list

--- core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
+++ core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
@@ -1,9 +1,12 @@
 package com.example.core.data.repository
 
 import com.example.core.domain.model.CharacterPaging
+import com.example.core.domain.model.Comic
 
 interface CharactersRemoteDataSource {
 
     suspend fun fetchCharacters(queries: Map<String, String>): CharacterPaging
+
+    suspend fun fetComics(characterId: Int): List<Comic>
 
 }
