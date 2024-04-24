f0e6eb7b9534fb78ba2caf9c9e54d1252df8c2b0
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-24 08:26:08 -0300

    Mapeamento dos objetos de response da api da marvel

---
+++ core/src/main/java/br/com/julianovince/core/data/network/response/DataWrapperResponse.kt
@@ -0,0 +1,6 @@
+package br.com.julianovince.core.data.network.response
+
+data class DataWrapperResponse(
+    val copyright: String,
+    val data: DataContainerResponse
+)
---
+++ core/src/main/java/br/com/julianovince/core/data/network/response/ThubmbnailResponse.kt
@@ -0,0 +1,6 @@
+package br.com.julianovince.core.data.network.response
+
+data class ThubmbnailResponse(
+    val path: String,
+    val extension: String
+)
