b3e58bbc24fb1f76717243c1a2eb02984b838d62
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-24 15:17:32 -0300

    Implementa repositorio na camada de framework

---
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.example.marvelapp.framework
+
+import androidx.paging.PagingSource
+import br.com.julianovince.core.data.repository.CharactersRemoteDataSource
+import br.com.julianovince.core.data.repository.CharactersRepository
+import br.com.julianovince.core.domain.model.Character
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import javax.inject.Inject
+
+class CharactersRepositoryImpl @Inject constructor(
+    private val remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+) : CharactersRepository{
+
+    override fun getCharacters(query: String): PagingSource<Int, Character> {
+        remoteDataSource.fetchCharacters(queries = query)
+    }
+
+}
