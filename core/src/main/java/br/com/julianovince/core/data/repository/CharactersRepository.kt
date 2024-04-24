0da793fc00bfcc4e63ea9ccf8d4b2ef15f20654d
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-24 15:02:32 -0300

    Cria interfaces para fonte de dados

---
+++ core/src/main/java/br/com/julianovince/core/data/repository/CharactersRepository.kt
@@ -0,0 +1,10 @@
+package br.com.julianovince.core.data.repository
+
+import androidx.paging.PagingSource
+import br.com.julianovince.core.domain.model.Character
+
+interface CharactersRepository {
+
+    fun getCharacters(query: String): PagingSource<Int,Character>
+
+}
